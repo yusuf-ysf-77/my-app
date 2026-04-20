0421e58548a45ea27ac1d73be12fcd7c749714ac
yusuf-ysf-77 <ysf12060@gmail.com>
2026-04-20 12:06:44 +0800

    feat: new learn page

---
+++ app/Learn/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Learnpage() {
+    return (
+    <div>
+        <div>
+            <img src="/palette.svg" alt="Design" />
+            <p>Design</p>
+        </div>
+    <div>
+        <p>FIGMA</p>
+        <p>8 Hour ago</p>
+    </div>
+    </div>
+        );
+}
